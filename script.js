020b72c06dd3cbc16000ae863335970b7f30a4e4
RoshanlalPrajapati <rooshanlal1200@gmail.com>
2022-03-23 11:12:50 +0530

    Updates

---
+++ script.js
@@ -0,0 +1,112 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What is an operating system?",
+    answer: "all of the mentioned",
+    options: [
+      "collections of program that manages hardware resources",
+      "interface between the hardware and spplications programs",
+      "system service provider to the application programs",
+      "all of the mentioned"
+    ]
+  },
+    {
+    numb: 2,
+    question: "what is the main function of the command interpreter?",
+    answer: "to get and execute the next user-specified command",
+    options: [
+      "to provide the interface between the API and application program",
+      "to handle the file in the operating system",
+      "to get and execute the next user-specified command",
+      "none of the mentioned"
+    ]
+  },
+    {
+    numb: 3,
+    question: "In Operating System which of the following is/are CPU scheduling algorithm?",
+    answer: "All of the mentioned",
+    options: [
+      "Priority",
+      "Round Robin",
+      "Shortest jon first",
+      "All of the mentioned"
+    ]
+  },
+    {
+    numb: 4,
+    question: "To access the services of the Operating System, the interface provided by the________?",
+    answer: "System call",
+    options: [
+      "Library",
+      "System call",
+      "Assemble instruction",
+      "API"
+    ]
+  },
+    {
+    numb: 5,
+    question: "CPU scheduling is the basic of______?",
+    answer: "Multiprogramming Operating System",
+    options: [
+      "Multiprogramming Operating System",
+      "Larger memory sized system",
+      "Multiprocessor System",
+      "none of the mentioned"
+    ]
+  },
+   {
+    numb: 6,
+    question: "What Operating System MacBook used",
+    answer: "iPhone Operating System",
+    options: [
+      "iApple Operating System",
+      "iNternational Operating System",
+      "iPhone Operating System",
+      "iNternet personal Operating System"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Which of the following is not true",
+    answer: "Kernel is made of various modules which cannot be loaded in running operating system",
+    options: [
+      "Kernels remains in the memory during the entire computer session",
+      "Kernel is made of various modules which cannot be loaded in running operating system",
+      "Kernel is the first part of the operating system to load into memory during booting ",
+      "Kernel is the program that consitutes the central core of the operating system"
+    ]  
+  },
+  {
+    numb: 8,
+    question: "Which one of the following error will be handle by the Operating Syatem",
+    answer: "All of the mentioned",
+    options: [
+      "Lack of paper in printer",
+      "Connection faliure in the network",
+      "Power Faliure",
+      "All of the mentioned"
+    ]  
+  },
+  {
+    numb: 9,
+    question: "Ia a process fails, most operating system write the error information to a_____",
+    answer: "Log file",
+    options: [
+      "New file",
+      "Another running process",
+      "Log file",
+      "None of of the mentioned"
+    ]  
+  },
+  {
+  numb: 10,
+    question: "Which one of the following is not a real time operating system",
+    answer: "Palm OS",
+    options: [
+      "RTLinux",
+      "Palm OS",
+      "QNX",
+      "VxWorks"
+    ]  
+  }
+];
